0ddf3f426b042fa042efb769755e4b3f7a8f353f
abdulnasirym <abdulnasiry05@gmail.com>
2024-11-12 23:04:24 +0000

    Authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -24,7 +24,10 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Gets the authorization header from the request """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Get the current user """
