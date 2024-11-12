2df43f8dc44e53efb376b9feea06692e5f1df45f
abdulnasirym <abdulnasiry05@gmail.com>
2024-11-12 22:28:19 +0000

    Authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,18 @@ class Auth:
     """ All authentication system implemented in this app"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Checks ig a path requires authentication"""
-        return False
+        if path is None:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        normalized_path = path if path.endswith('/') else path + '/'
+
+        if normalized_path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Gets the authorization header from the request """
