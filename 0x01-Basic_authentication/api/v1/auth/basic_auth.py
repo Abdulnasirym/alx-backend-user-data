8ad1614e6b7bebe40d936d94da6a6e93eaa9ded6
abdulnasirym <abdulnasiry05@gmail.com>
2024-11-12 23:48:53 +0000

    Added a method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,7 +7,8 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ BasicAuth classinheriting from Auth """
     def extract_base64_authorization_header(
-        self, authorization_header: str) -> str:
+            self, authorization_header: str) -> str:
+        """Extract the base64 part of the Authorized header"""
         if authorization_header is None:
             return None
 
