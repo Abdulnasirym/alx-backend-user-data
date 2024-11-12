4aa0a1e3bb7cdccf2b4d00c5a3973c8d8f1cce5d
abdulnasirym <abdulnasiry05@gmail.com>
2024-11-12 23:58:11 +0000

    Added a new method for decoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """ BasicAuth module """
 
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -19,3 +21,23 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split("Basic ")[1].strip()
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decodes the Base64 string `base64_authorization_header` and
+        returns the decoded value
+        """
+        if base64_authorization_header is None:
+            return None
+       
+        if not isinstance(base64_authorization_header, str):
+            return None
+        
+        try:
+            decoded = base64.b64decode(
+                base64_authorization_header,
+                validate=True
+            )
+            return decoded.decode('utf-8')
+        except (binascii.Error, UnicodeDecodeError):
+            return None
