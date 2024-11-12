957b418379b7d7d2693c31417a697ef60a984a3b
abdulnasirym <abdulnasiry05@gmail.com>
2024-11-12 19:51:22 +0000

    Adjusted

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
+def unauthorized() -> None:
     """ GET /api/v1/stats
     Return:
        - unauthorized access
