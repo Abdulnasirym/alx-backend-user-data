baeba95025e6f65c005a9a9838d95b5efe7fe937
abdulnasirym <abdulnasiry05@gmail.com>
2024-11-22 16:26:53 +0000

    a SQLAlchemy model named User for a database table named users

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 """ A module that create a user model """
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, create_engine
 
 Base = declarative_base()
 
@@ -12,6 +12,10 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+
+engine = create_engine('sqlite:///test.db')
+Base.metadata.create_all(engine)
